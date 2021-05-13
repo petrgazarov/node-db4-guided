14de3fc512aac4fc7a180e63dc04e44c3b92eff1
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-05-13 19:00:07 +0200

    wip

--- data/migrations/20210513165737_first-migration.js
+++ data/migrations/20210513165737_first-migration.js
@@ -1,6 +1,8 @@
 exports.up = async function(knex) {
-  await knex.schema.createTable('zoos')
-    
+  await knex.schema.createTable('zoos', tbl => {
+    tbl.increments('zoo_id')
+  })
+
 };
 
 exports.down = function(knex) {
