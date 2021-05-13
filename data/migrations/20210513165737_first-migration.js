559ba64b23d03571dc0b0f0bec5f3de4c722cd63
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-05-13 19:04:06 +0200

    wip

--- data/migrations/20210513165737_first-migration.js
+++ data/migrations/20210513165737_first-migration.js
@@ -3,7 +3,7 @@ exports.up = async function (knex) {
     .createTable('zoos', tbl => {
       tbl.increments('zoo_id')
       tbl.string('zoo_name', 30).notNullable().unique()
-      tbl.string('address', 50).notNullable()
+      tbl.string('address', 50).notNullable().unique()
     })
     .createTable('species', tl)
 
