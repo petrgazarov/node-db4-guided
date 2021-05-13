696d44e2eeaf7debf060b77650b069312d785eb5
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-05-13 19:39:39 +0200

    wip

--- data/seeds/01-zoos.js
+++ data/seeds/01-zoos.js
@@ -1,5 +1,5 @@
 
-exports.seed = async function(knex, Promise) {
+exports.seed = async function(knex) {
   // await knex('zooz').truncate()
   await knex('zoos').insert([
     { zoo_name: 'San Diego Zoo', address: "2920 Zoo Dr, San Diego, CA 92101" },
