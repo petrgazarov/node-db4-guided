9c729cc05151ee03bc6662259bea3cd7a1bd4bb6
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-05-13 19:15:58 +0200

    wip

--- data/migrations/20210513165737_first-migration.js
+++ data/migrations/20210513165737_first-migration.js
@@ -17,6 +17,7 @@ exports.up = async function (knex) {
         .notNullable()
         .references('species_id')
         .inTable('species')
+        .onDelete('CASCADE')
     })
     .createTable('', tbl => {
 
