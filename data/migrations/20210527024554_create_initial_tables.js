94add62a9fdd9add138fca835f1fbb8f7753ac3a
Petr Gazarov <petrgazarov@gmail.com>
2021-05-26 23:04:51 -0700

    Add code and notes

---
+++ data/migrations/20210527024554_create_initial_tables.js
@@ -0,0 +1,163 @@
+//----------------------------------------------------------------------------//
+// CREATE TABLES AND FOREIGN KEY RELATIONSHIPS
+//----------------------------------------------------------------------------//
+// Tables can have 1-to-1, 1-to-many, or many-to-many "relationships". These
+// relationships provide value through the ability to 'JOIN' tables. By using a
+// primary key value from Table A as the value of a foreign key field in Table
+// B, you "relate" the two records, and using JOIN statements, you can combine
+// the related data in a SQL query result.
+//
+// When creating tables, you should create tables that do not have any foreign
+// keys *first*. These are tables that are likely to be "parent" (a.k.a.
+// "primary") tables that are referred to by other tables (a.k.a. "child"
+// tables). For example, if you try to create the [animals] table below, with a
+// foreign key field that references the [species] table, but the [species]
+// table hasn't been created yet, you will get an error. The [species] table
+// needs to be created before you can define a foreign key field in [animals]
+// that references it.
+exports.up = function(knex) {
+  return knex.schema
+    // Since the [species] table is referenced by the [animals] table,
+    // the [species] table needs to be created first.
+    .createTable('species', tbl => {
+      // Remember that .increments() creates an integer field that is
+      // auto-incrementing, not nullable, and unique, and that is
+      // specified as the "primary" key field for the table.
+      tbl.increments('species_id');
+      tbl.string('species_name').notNullable().unique();
+    })
+    .createTable('animals', tbl => {
+      tbl.increments('animal_id');
+      tbl.string('animal_name').notNullable().unique();
+      //------------------------------------------------------------//
+      // DEFINING A FOREIGN KEY FIELD USING KNEX - ENSURING
+      // REFERENTIAL INTEGRITY
+      //------------------------------------------------------------//
+      // The "species_id" field is a foreign key field. There are
+      // multiple ways to define a foreign key field using knex. They
+      // all have these things in common:
+      //
+      //   1. The field definition identifies the "parent" table, and
+      //      the primary key field or column in that parent table
+      //      that the foreign key field referenes.
+      //   2. The field type must match the field type of the primary
+      //      key field in the primary table. Primary keys that are
+      //      integers are always *unsigned* (which means that they
+      //      are never negative numbers). When defining a matching
+      //      foreign key field that references a primary key
+      //      that is an integer, be sure to make it .unsigned().
+      //   3. The default behavior of the database when a record is
+      //      deleted from the primary table, and the primary key for
+      //      that record is used as a foreign key value in other
+      //      "child" tables, is to disallow the delete request and
+      //      throw an error. This is also true if the primary key
+      //      value is *modified* (or updated) - changing a primary
+      //      key value has the same effect as deleting the record,
+      //      and creating a new record with a different primary key
+      //      value. You can override this default "disallow" behavior
+      //      by using the .onDelete() and .onUpdate() methods. The
+      //      string values you can pass to these methods are outlined
+      //      below, where .onDelete() and .onUpdate() are called. See
+      //      documentation for the "foreign()" method at
+      //      https://knexjs.org.
+      tbl
+        .integer('species_id')
+        .references('species.species_id')
+        .notNullable()
+        .unsigned()
+        //--------------------------------------------------------//
+        // VALUES FOR .onDelete() AND .onUpdate()
+        //--------------------------------------------------------//
+        // The .onDelete() and .onUpdate() methods allow us to
+        // control how the database handles delete and update
+        // requests for records with primary keys that are used (or
+        // "referred to") by child tables. The following values can
+        // be passed in to .onDelete() and .onUpdate():
+        //
+        //   * RESTRICT - this is the default if you don't call
+        //     .onDelete() or .onUpdate(). You may choose to call
+        //     those methods and pass 'RESTRICT' anyway, just to
+        //     make it clear that you intend for that to be the
+        //     behavior (as opposed to just forgetting what the
+        //     default behavior is, or unknowingly accepting the
+        //     default.)
+        //   * CASCADE - this causes the database to find and delete
+        //     all records in child tables that reference the
+        //     primary record being deleted. The delete request in
+        //     the primary table is "cascaded" to all child tables
+        //     that reference the priamary record's key. This is the
+        //     most destructive option.
+        //   * SET NULL - this causes the database to find all
+        //     records in child tables that reference the primary
+        //     record being deleted, and to set their foreign key
+        //     field values to "null" (for the foreign key that
+        //     references the record being deleted.) This helps
+        //     ensure that there are no child records that refer to the
+        //     primary record being deleted. NOTE: This could have
+        //     unintended consequences if you have defined a foreign
+        //     key field as .notNullable()... be careful!
+        //   * NO ACTION - this causes the database to allow the
+        //     deletion of the primary record, without taking any
+        //     other action. This will break referential
+        //     integrity! You will have records in child tables
+        //     that are referring to a record in a parent table
+        //     *that does not exist*. It is rarely an appropriate
+        //     solution.
+        //
+        // Here, we set the delete and update behavior for parent
+        // tables to 'CASCADE'.
+        //
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+    })
+    .createTable('zoos', tbl => {
+      tbl.increments('zoo_id');
+      tbl.string('zoo_name').notNullable().unique();
+      tbl.string('address').notNullable();
+    })
+    //----------------------------------------------------------------//
+    // CREATE A BRIDGE TABLE FOR THE MANY-TO-MANY RELATIONSHIP
+    //----------------------------------------------------------------//
+    // In our example, zoos and animals are related, but there can be
+    // many zoos for any given animal, and of course, many animals for
+    // any given zoo. Since a foreign key field can only have 1 value,
+    // putting foreign key fields in each table won't work. We need a
+    // third table. For us, it's the [zoo_animals] table. Bridge tables
+    // are often named using the names of the 2 tables that the bridge
+    // is for, but it is not always the case.
+    //
+    // This table will have 2 foreign key fields, one for each of the
+    // tables that are being related. Each record in the bridge table
+    // will represent a relationship between a record in each parent
+    // table.
+    //
+    .createTable('zoo_animals', tbl => {
+      tbl
+        .integer('zoo_id')
+        .references('zoos.zoo_id')
+        .notNullable()
+        .unsigned()
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+      tbl
+        .integer('animal_id')
+        .references('animals.animal_id')
+        .notNullable()
+        .unsigned()
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+      // This is how we can make a composite primary key -
+      // the combination of the two keys becomes our primary key.
+      // This will enforce unique combinations of ids.
+      tbl.primary(['zoo_id', 'animal_id']);
+    });
+};
+
+exports.down = function(knex) {
+  // Drop in the opposite order to avoid errors related to FK constraints
+  return knex.schema
+    .dropTableIfExists('zoo_animals')
+    .dropTableIfExists('animals')
+    .dropTableIfExists('zoos')
+    .dropTableIfExists('species');
+};
