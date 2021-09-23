608212c0994c441d500f876f45d1632ece8ac0f8
Petr Gazarov <petrgazarov@gmail.com>
2021-09-22 20:35:04 -0700

    Add migration logic; fix table name bug

--- data/migrations/20210923025017_create_initial_tables.js
+++ data/migrations/20210923025017_create_initial_tables.js
@@ -2,22 +2,50 @@
 exports.up = function(knex) {
   return knex.schema
     .createTable('zoos', table => {
-
+      table.increments('zoo_id');
+      table.string('zoo_name').notNullable().unique();
+      table.string('address').notNullable();
     })
     .createTable('species', table => {
-      
+      table.increments('species_id');
+      table.string('species_name').notNullable();
     })
     .createTable('animals', table => {
-      
+      table.increments('animal_id');
+      table.string('animal_name');
+      table
+        .integer('species_id')
+        .unsigned()
+        .references('species_id')
+        .inTable('species')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
     })
-    .createTable('animals_zoos', table => {
+    .createTable('zoo_animals', table => {
+      table.increments('zoo_animal_id');
+      table.bool('is_current');
 
+      table
+        .integer('zoo_id')
+        .unsigned()
+        .references('zoo_id')
+        .inTable('zoos')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+
+      table
+        .integer('animal_id')
+        .unsigned()
+        .references('animal_id')
+        .inTable('animals')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
     });
 };
 
 exports.down = function(knex) {
   return knex.schema
-    .dropTableIfExists('animals_zoos')
+    .dropTableIfExists('zoo_animals')
     .dropTableIfExists('animals')
     .dropTableIfExists('species')
     .dropTableIfExists('zoos')
